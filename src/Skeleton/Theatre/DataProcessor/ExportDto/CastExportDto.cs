aed15f1b0f9e0bdf35a3b2439a4a6547f6e6ba1a
Dobri <dobry_georgiev@abv.bg>
2021-12-04 17:12:01 +0200

    export commite

---
+++ src/Skeleton/Theatre/DataProcessor/ExportDto/CastExportDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Theatre.Validation;
+
+namespace Theatre.DataProcessor.ExportDto
+{
+    public class CastExportDto
+    {
+        [Required]
+        [StringLength(ValidationConstraints.Cast.FullNameMaxLength, MinimumLength = ValidationConstraints.Play.TitleMinLength)]
+        public string FullName { get; set; }
+
+        [Required]
+        public bool IsMainCharacter { get; set; }
+
+        [Required]
+
+        public string MainCharacter { get; set; };
+    }
+}
--- src/Skeleton/Theatre/DataProcessor/Serializer.cs
+++ src/Skeleton/Theatre/DataProcessor/Serializer.cs
@@ -4,7 +4,10 @@
     using Newtonsoft.Json;
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
     using Theatre.Data;
     using Theatre.DataProcessor.ExportDto;
 
@@ -49,14 +52,57 @@
                 theatres.Add(exportTheatre);
             }
 
-            theatres = (List<TheatreExportDto>)theatres.OrderByDescending(t => t.NumberOfHalls).ThenBy(t => t.Name);
+            theatres = theatres.OrderByDescending(t => t.NumberOfHalls).ThenBy(t => t.Name).ToList();
 
             return JsonConvert.SerializeObject(theatres);
         }
 
         public static string ExportPlays(TheatreContext context, double rating)
         {
-            throw new NotImplementedException();
+            List<PlaysExportDto> plays = new List<PlaysExportDto>();
+
+            foreach (var play in context.Plays)
+            {
+
+                if (play.Rating > rating)
+                {
+                    continue;
+                }
+
+             PlaysExportDto exportPlay = new PlaysExportDto()
+             { 
+                Title = play.Title,
+                Duration = play.Duration,
+                Rating = play.Rating,
+                Genre = play.Genre
+             };
+                List<CastExportDto> casts = new List<CastExportDto>();
+                foreach (var cast in play.Casts)
+                {
+                    if (cast.IsMainCharacter)
+                    {
+                        casts.Add(new CastExportDto
+                        {
+                            FullName = cast.FullName,
+                            MainCharacter = "Plays main character in '" + play.Title + "'." 
+                        });
+                    }
+                }
+                casts = casts.OrderByDescending(c => c.FullName).ToList();
+                exportPlay.Casts = (ICollection<Data.Models.Cast>)casts;
+                plays.Add(exportPlay);
+            }
+            plays = plays.OrderBy(p => p.Title).ThenByDescending(p => p.Genre).ToList();
+
+            XmlRootAttribute root = new XmlRootAttribute("Plays");
+            XmlSerializer serializer = new XmlSerializer(typeof(PlaysExportDto[]), root);
+            StringBuilder sb = new StringBuilder();
+
+            using (StringWriter sw = new StringWriter(sb))
+            {
+                serializer.Serialize(sw, plays);
+            }
+            return sb.ToString();
         }
     }
 }
