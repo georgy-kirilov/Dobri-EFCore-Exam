a54df49d39566b7d4076a089716a258f3f0a1115
Dobri <dobry_georgiev@abv.bg>
2021-12-04 16:32:19 +0200

    json export

---
+++ src/Skeleton/Theatre/DataProcessor/ExportDto/TicketExportDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Theatre.Validation;
+
+namespace Theatre.DataProcessor.ExportDto
+{
+    public class TicketExportDto
+    {
+        [Range((double)ValidationConstraints.Ticket.MinPrice, (double)ValidationConstraints.Ticket.MaxPrice)]
+        public decimal Price { get; set; }
+
+        [Required]
+        //range 1-10
+        [Range(ValidationConstraints.Ticket.MinRowNumber, ValidationConstraints.Ticket.MaxRowNumber)]
+        public sbyte RowNumber { get; set; 
+
+    }
+}
--- src/Skeleton/Theatre/DataProcessor/Serializer.cs
+++ src/Skeleton/Theatre/DataProcessor/Serializer.cs
@@ -1,13 +1,57 @@
 ﻿namespace Theatre.DataProcessor
 {
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using Theatre.Data;
+    using Theatre.DataProcessor.ExportDto;
 
     public class Serializer
     {
         public static string ExportTheatres(TheatreContext context, int numbersOfHalls)
         {
-            throw new NotImplementedException();
+            List<TheatreExportDto> theatres = new List<TheatreExportDto>();
+
+            foreach (var theatre in context.Theatres)
+            {
+                if (theatre.NumberOfHalls < numbersOfHalls)
+                {
+                    continue;
+                }
+
+                TheatreExportDto exportTheatre = new TheatreExportDto() {
+                    Name = theatre.Name,
+                    NumberOfHalls = theatre.NumberOfHalls,
+                };
+
+                List<TicketExportDto> tickets = new List<TicketExportDto>();
+                decimal TotalIncome = 0;
+
+                foreach (var ticket in theatre.Tickets)
+                {
+                    if (ticket.RowNumber >= 1 && ticket.RowNumber <= 5)
+                    {
+                        tickets.Add(new TicketExportDto
+                        {
+                            RowNumber = ticket.RowNumber,
+                            Price = ticket.Price
+                        }
+                        );
+                        TotalIncome += ticket.Price;
+                    }
+                }
+                exportTheatre.TotalIncome = TotalIncome;
+                tickets.OrderByDescending(t => t.Price);
+                exportTheatre.Tickets = (ICollection<Data.Models.Ticket>)tickets;
+
+                theatres.Add(exportTheatre);
+            }
+
+            theatres = (List<TheatreExportDto>)theatres.OrderByDescending(t => t.NumberOfHalls).ThenBy(t => t.Name);
+
+            return JsonConvert.SerializeObject(theatres);
         }
 
         public static string ExportPlays(TheatreContext context, double rating)
