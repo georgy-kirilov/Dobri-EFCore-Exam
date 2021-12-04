77b991812be2fdd4318a23d1a28b8672f27a748a
Dobri <dobry_georgiev@abv.bg>
2021-12-04 14:19:59 +0200

    connection string const for dobri added

---
+++ src/Skeleton/Theatre/GlobalConst.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Theatre
+{
+    public class GlobalConst
+    {
+        public string dbConnectionStringDobri = "Server=.;Database=VaporStore;User Id=sa;Password=DobriSoftUni2021;";
+    }
+}
